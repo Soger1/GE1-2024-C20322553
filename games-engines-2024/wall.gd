0d61221bba132ad7d1b1166acf50980fef36b71b
Ethan <ethanholohan1@gmail.com>
2024-10-04 14:06:43 +0100

    housekeeptin

--- games-engines-2024/wall.gd
+++ games-engines-2024/wall.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var brick_scene: PackedScene
 
 @export var row = 10
-@export var col = 250
+@export var col = 25
 
 
 # Called when the node enters the scene tree for the first time.
